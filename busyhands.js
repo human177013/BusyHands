ec1483dada22c9467f59a4a5759562199e7e87b9
human177013 <pjo6493@gmail.com>
2022-06-25 05:11:01 -0400

    moved async page load logic to main script

--- busyhands.js
+++ busyhands.js
@@ -1,1 +1,39 @@
-console.log("Test Loaded!!!!!!!!")
+const LOAD_INIT_WAIT = 0;
+const LOAD_TIMEOUT = 5000;
+
+let startTime = undefined;
+
+function waitPageLoad() {
+    return new Promise(resolve => {
+        setTimeout((_document) => {
+            // console.log("Checking document.readyState")
+            if (_document.readyState !== 'complete' && !((Date.now() - startTime) > LOAD_TIMEOUT)) {
+                // console.log('Document not ready');
+                resolve(waitPageLoad());
+            } else {
+                console.log('Document ready');
+                resolve(_document.readyState);
+            }
+        }, LOAD_INIT_WAIT, document);
+    });
+}
+
+async function start() {
+    try {
+        let fetchAttempt = 0
+        startTime = Date.now();
+        let docState = await waitPageLoad()
+
+        if (docState !== 'complete') {
+            throw new Error("Load Timeout: Page did noy load in a timely manner.")
+        } else {
+            console.log('BusyHands Started');
+        }
+        
+    } catch (error) {
+        console.log('BusyHands failed');
+        console.error(error);
+    }
+}
+
+start()
--- script.user.js
+++ script.user.js
@@ -14,37 +14,14 @@
 
 const URL = "https://human177013.github.io/BusyHands/busyhands.js";
 const FETCH_MAX_ATTEMPT = 3;
-const LOAD_INIT_WAIT = 0;
-const LOAD_TIMEOUT = 5000;
-
-let startTime = undefined;
 
 
-function waitPageLoad() {
-    return new Promise(resolve => {
-        setTimeout((_document) => {
-            // console.log("Checking document.readyState")
-            if (_document.readyState !== 'complete' && !((Date.now() - startTime) > LOAD_TIMEOUT)) {
-                // console.log('Document not ready');
-                resolve(waitPageLoad());
-            } else {
-                console.log('Document ready');
-                resolve(_document.readyState);
-            }
-        }, LOAD_INIT_WAIT, document);
-    });
-}
 
-async function loadScript() {
+
+(function() {
     try {
         let fetchAttempt = 0
-        startTime = Date.now();
-        let docState = await waitPageLoad()
-
-        if (docState !== 'complete') {
-            throw new Error("Load Timeout: Page did noy load in a timely manner.")
-        }
-
+        
         fetch(URL).then((response) => {
             response.text().then((script) => {
                 window.eval(script);
@@ -62,6 +39,4 @@ async function loadScript() {
         console.log('BusyHands failed to load!');
         console.error(error);
     }
-}
-
-loadScript();
+})()
