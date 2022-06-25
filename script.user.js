aa56b80fe47faf8ae5073cb9f845a3e444456fd6
human177013 <pjo6493@gmail.com>
2022-06-25 04:02:56 -0400

    fix script loader

--- script.user.js
+++ script.user.js
@@ -14,31 +14,50 @@
 
 const URL = "https://human177013.github.io/BusyHands/busyhands.js";
 const FETCH_MAX_ATTEMPT = 3;
-const LOAD_TIMEOUT = 30000;
+const LOAD_INIT_WAIT = 0;
+const LOAD_TIMEOUT = 5000;
+
+let startTime = undefined;
 
 
-async function loadScript() { 
+function waitPageLoad() {
+    return new Promise(resolve => {
+        setTimeout((_document) => {
+            // console.log("Checking document.readyState")
+            if (_document.readyState !== 'complete' && !((Date.now() - startTime) > LOAD_TIMEOUT)) {
+                // console.log('Document not ready');
+                resolve(waitPageLoad());
+            } else {
+                console.log('Document ready');
+                resolve(_document.readyState);
+            }
+        }, LOAD_INIT_WAIT, document);
+    });
+}
+
+async function loadScript() {
     try {
-        var fetchAttempt = 0
-        var startTime = Date.now()
-        
+        let fetchAttempt = 0
+        startTime = Date.now();
+        let docState = await waitPageLoad()
+
+        if (docState !== 'complete') {
+            throw new Error("Load Timeout: Page did noy load in a timely manner.")
+        }
+
         fetch(URL).then((response) => {
             response.text().then((script) => {
-                while (document.readyState !== 'complete') {
-                    if ((Date.now() - startTime) > 30000) {
-                        throw new Error("Load Timeout: Page did noy load in a timely manner.")
-                    }
-                }
-                window.eval(script);                
+                window.eval(script);
             });
         }).catch((error) => {
             fetchAttempt++;
-            if (fetchAttempt > FETCH_MAX_ATTEMPT ) {
+            if (fetchAttempt > FETCH_MAX_ATTEMPT) {
                 throw error;
             } else {
-                console.log('BusyHands script failed to load '+ fetchAttempt + ' times.');
+                console.log('BusyHands script failed to load ' + fetchAttempt + ' times.');
             }
         });
+        
     } catch (error) {
         console.log('BusyHands failed to load!');
         console.error(error);
