8d214fa206673db785513919134392ac6d83cecd
human177013 <pjo6493@gmail.com>
2022-10-26 19:39:54 -0400

    restructure

---
+++ scripts/headers/busyhands-dev_nhentai.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         BusyHands-dev nhentai
+// @namespace    https://human177013.github.io
+// @version      1.1.2-dev
+// @icon         https://i.imgur.com/uLAimaY.png
+// @description  nhentai one-handed tool
+// @author       human177013
+// @match        https://nhentai.net/*
+// @website      https://human177013.github.io/BusyHands/
+// @updateURL    https://human177013.github.io/BusyHands/scripts/headers/busyhands-dev_nhentai.meta.js
+// @downloadURL  https://human177013.github.io/BusyHands/scripts/headers/busyhands-dev_nhentai.user.js
+// @require      https://raw.githubusercontent.com/human177013/BusyHands/develop/scripts/busyhands_loader.js
+// @require      https://raw.githubusercontent.com/human177013/BusyHands/develop/scripts/busyhands_nhentai.js
+// @run-at       document-end
+// @grant        none
+// ==/UserScript==
---
+++ scripts/headers/busyhands-dev_nhentai.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         BusyHands-dev nhentai
+// @namespace    https://human177013.github.io
+// @version      1.1.2-dev
+// @icon         https://i.imgur.com/uLAimaY.png
+// @description  nhentai one-handed tool
+// @author       human177013
+// @match        https://nhentai.net/*
+// @updateURL    https://human177013.github.io/BusyHands/scripts/headers/busyhands-dev_nhentai.meta.js
+// @downloadURL  https://human177013.github.io/BusyHands/scripts/headers/busyhands-dev_nhentai.user.js
+// @require      https://raw.githubusercontent.com/human177013/BusyHands/develop/scripts/busyhands_loader.js
+// @require      https://raw.githubusercontent.com/human177013/BusyHands/develop/scripts/busyhands_nhentai.js
+// @run-at       document-end
+// @grant        none
+// ==/UserScript==
